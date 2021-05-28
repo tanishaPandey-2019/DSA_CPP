26f4cf69409ec2ed621e02960b0e854e303b4740
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2021-05-28 18:27:00 +0530

    Time: 8 ms (59.11%), Space: 8.7 MB (8.91%) - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -12,20 +12,50 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         
-        ListNode* prev = NULL;
-        ListNode* current = head;
-        ListNode* second = head;
+        stack<int> s;
         
-        while(second != NULL)
+        ListNode* temp = head;
+        while(temp != NULL)
         {
-            second = second->next;
-            current->next = prev;
-            prev = current;
-            current = second;
+            s.push(temp->val);
+            temp = temp->next;
         }
-        head = prev;
         
-        return head;
+        if(head == NULL)
+        {
+            return head;
+        }
+        ListNode* newNode = new ListNode;
+        newNode->val = s.top();
+        newNode->next = NULL;
+        s.pop();
+        ListNode* newHead = newNode;
+        while(!s.empty())
+        {
+            ListNode* node = new ListNode;
+            node->val = s.top();
+            node->next = NULL;
+            s.pop();
+            newNode->next = node;
+            newNode = node;
+        }
+        
+        return newHead;
+        
+//         ListNode* prev = NULL;
+//         ListNode* current = head;
+//         ListNode* second = head;
+        
+//         while(second != NULL)
+//         {
+//             second = second->next;
+//             current->next = prev;
+//             prev = current;
+//             current = second;
+//         }
+//         head = prev;
+        
+//         return head;
         
 //         ListNode* first = NULL;
 //         ListNode* present = head;
