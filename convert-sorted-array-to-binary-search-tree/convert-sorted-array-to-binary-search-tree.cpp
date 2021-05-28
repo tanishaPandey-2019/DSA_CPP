006dd65afd1c50ad37c02b50737c3b86a6df6192
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2021-05-29 01:17:02 +0530

    Time: 12 ms (70.30%), Space: 21.4 MB (42.48%) - LeetHub

--- convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -10,24 +10,35 @@
  * };
  */
 class Solution {
+    TreeNode* sortedArrayToBST(vector<int>& nums, int start, int end){
+        if(end<=start) return NULL; 
+        int midIdx=(end+start)/2;
+        TreeNode* root=new TreeNode(nums[midIdx]);
+        root->left=sortedArrayToBST(nums, start, midIdx);
+        root->right=sortedArrayToBST(nums, midIdx+1,end);
+        return root;
+    }
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        
-        if(nums.size() == 0)
-            return NULL;
-        if(nums.size() == 1)
-            return new TreeNode(nums[0]);
-        
-        int mid = nums.size()/2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        
-        vector<int> leftInt(nums.begin(), nums.begin() + mid);
-        vector<int> rightInt(nums.begin() + mid + 1,nums.end());
-        
-        root->left = sortedArrayToBST(leftInt);
-        root->right = sortedArrayToBST(rightInt);
-        
-        return root;
-        
+        return sortedArrayToBST(nums, 0,nums.size());
     }
 };
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+//         if(nums.size() == 0)
+//             return NULL;
+//         if(nums.size() == 1)
+//             return new TreeNode(nums[0]);
+        
+//         int mid = nums.size()/2;
+//         TreeNode* root = new TreeNode(nums[mid]);
+        
+//         vector<int> leftInt(nums.begin(), nums.begin() + mid);
+//         vector<int> rightInt(nums.begin() + mid + 1,nums.end());
+        
+//         root->left = sortedArrayToBST(leftInt);
+//         root->right = sortedArrayToBST(rightInt);
+        
+//         return root;
+        
+    
