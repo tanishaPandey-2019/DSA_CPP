48e45c4a0b2a345e0d89dfd061f5c3f5e04e7165
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2021-10-03 00:35:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.92%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+vector<int> dp(38,-1);
+class Solution {
+public:
+    
+    int tribonacci(int n) {
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 2] + dp[i - 1] + dp[i - 3];
+        }
+        
+        return dp[n];
+        
+    }
+};
