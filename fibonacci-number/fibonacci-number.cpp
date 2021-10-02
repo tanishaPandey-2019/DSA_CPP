1d87ce4fcc44271e7496ccacccdbf48e67c6b6c8
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2021-10-03 00:25:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.55%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -4,16 +4,14 @@ public:
     
     int fib(int n) {
         
-        if(n == 0)
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
         {
-            return 0;
-        }
-        if(n == 1)
-        {
-            return 1;
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         
-        return fib(n - 1) + fib(n - 2);
-        
+        return dp[n];
     }
 };
