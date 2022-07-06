75bb8de055a5e5835c393f1a1f106e15c66bf914
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2022-07-07 02:15:27 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (48.47%) - LeetHub

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -11,22 +11,28 @@
  */
 class Solution {
 public:
-    bool isMirror(TreeNode* root1, TreeNode* root2)
+    bool isSymmetricUtil(TreeNode* root1, TreeNode* root2)
     {
         if(root1 == NULL && root2 == NULL)
+        {
             return true;
-        
+        }
         if(root1 == NULL || root2 == NULL)
+        {
             return false;
+        }
         
-        return (root1->val == root2->val) && isMirror(root1->right,root2->left) &&          
-            isMirror(root1->left, root2->right);
+       
+        return (root1->val == root2->val) && isSymmetricUtil(root1->left,root2->right) && isSymmetricUtil(root1->right,root2->left);
+        
     }
     bool isSymmetric(TreeNode* root) {
         
-       return isMirror(root,root);
+        TreeNode* root1 = root;
+        TreeNode* root2 = root;
         
-        
+        return isSymmetricUtil(root1, root2);
+              
         
         
     }
