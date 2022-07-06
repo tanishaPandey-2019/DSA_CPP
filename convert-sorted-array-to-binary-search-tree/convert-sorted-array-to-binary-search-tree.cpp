a8ceb65a373196456ae1b427cd4919cc4526acf5
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2022-07-07 03:11:50 +0530

    Time: 16 ms (79.63%), Space: 21.5 MB (40.84%) - LeetHub

--- convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -10,35 +10,27 @@
  * };
  */
 class Solution {
-    TreeNode* sortedArrayToBST(vector<int>& nums, int start, int end){
-        if(end<=start) return NULL; 
-        int midIdx=(end+start)/2;
-        TreeNode* root=new TreeNode(nums[midIdx]);
-        root->left=sortedArrayToBST(nums, start, midIdx);
-        root->right=sortedArrayToBST(nums, midIdx+1,end);
+public:
+    TreeNode* solve(vector<int> &nums, int low, int high)
+    {
+        if(low > high)
+        {
+            return NULL;
+        }
+        
+        
+        int mid = (low + high)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = solve(nums, low, mid - 1);
+        root->right = solve(nums, mid + 1, high);
+        
         return root;
     }
-public:
+    
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBST(nums, 0,nums.size());
+        
+        TreeNode* root = solve(nums, 0, nums.size() - 1);
+        return root;
+        
     }
 };
-//     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        
-//         if(nums.size() == 0)
-//             return NULL;
-//         if(nums.size() == 1)
-//             return new TreeNode(nums[0]);
-        
-//         int mid = nums.size()/2;
-//         TreeNode* root = new TreeNode(nums[mid]);
-        
-//         vector<int> leftInt(nums.begin(), nums.begin() + mid);
-//         vector<int> rightInt(nums.begin() + mid + 1,nums.end());
-        
-//         root->left = sortedArrayToBST(leftInt);
-//         root->right = sortedArrayToBST(rightInt);
-        
-//         return root;
-        
-    
