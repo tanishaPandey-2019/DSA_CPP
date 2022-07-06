5b475e882d20a4f025c51e0929137ead89763f5a
tanishaPandey-2019 <68120087+tanishaPandey-2019@users.noreply.github.com>
2022-07-07 01:58:37 +0530

    Time: 8 ms (12.41%), Space: 10.1 MB (44.45%) - LeetHub

--- same-tree/same-tree.cpp
+++ same-tree/same-tree.cpp
@@ -11,40 +11,29 @@
  */
 class Solution {
 public:
-//     void inorder(TreeNode* root, vector<int> &ele)
-//     {
-//         if(root == NULL)
-//             return;
-        
-//         inorder(root->left, ele);
-//         ele.push_back(root->val);
-//         inorder(root->right,ele);
-        
-//     }
-    
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
         if(p == NULL && q == NULL)
+        {
             return true;
+        }
         
-        if(p == NULL || q == NULL)
+        if(p == NULL && q != NULL)
+        {
             return false;
+        }
+        
+        if(q == NULL && p != NULL)
+        {
+            return false;
+        }
         
         if(p->val != q->val)
+        {
             return false;
+        }
         
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        
-//         vector<int> pInorder;
-//         vector<int> qInorder;
-        
-//         inorder(p, pInorder);
-//         inorder(q, qInorder);
-        
-//         if(pInorder == qInorder)
-//             return true;
-//         else
-//             return false;
         
     }
 };
